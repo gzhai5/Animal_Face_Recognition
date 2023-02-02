967e0bb95fcd046e8bd29f0fc6a635cccbd3828d
guangxun <gzhai5@illinois.edu>
2023-02-02 18:29:23 -0500

    modify dockerfile to nginx version; create docker_compose strutures (.yml and startall script)

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,3 @@
-FROM --platform=linux/arm64/v8 ubuntu:latest
-
-RUN apt-get update
-RUN apt install -y nano wget
-RUN apt install -y nginx
-RUN apt-get install -y php7.4
-RUN apt install -y vim
-
-CMD service nginx restart
-CMD service php restart
+FROM nginx
 
 EXPOSE 80
---
+++ start_all.sh
@@ -0,0 +1,36 @@
+docker-compose -f docker_compose.yml down
+while getopts "bk" arg
+do
+  case $arg in
+    k)
+      echo "Stopped!"
+      exit 0
+      ;;
+    b)
+      docker-compose -f docker_compose.yml rm -f
+      BUILD="--build"
+      ;;
+    ?)
+      echo "Unknown args $arg... exit..."
+      exit 1
+      ;;
+  esac
+done
+
+# Remove previouse environemnt vars files, and Set new environment args (abs dir of the .sh no matter where run .sh)
+ENV_FILE=".env"
+rm -f $ENV_FILE
+CUR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+HOST_IP=$(ifconfig | grep "inet " | grep -v 127.0.0.1 | awk '{print $2}' | head -1)
+
+function encode_env {
+    echo "$1=${!1}" >> $ENV_FILE
+}
+encode_env "CUR_DIR"
+encode_env "HOST_IP"
+
+# Start service
+docker-compose -f docker_compose.yml up $BUILD -d
+
+# Open browser webpages
+open http://localhost:8083
